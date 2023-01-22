dd4e9a4da99458edeb25936b6a91bccd54368e0c
rgillespie23 <rgillespie@uchicago.edu>
2023-01-22 00:28:25 +0000

    End of Video

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,13 @@ class ApplicationController < ActionController::Base
     @square_of_num = @num ** 2
     render({ :template => "calculation_templates/square_results.html.erb"})
   end
+
+  def calculate_random
+
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+
+    render({ :template => "calculation_templates/random_results.html.erb"})
+  end
 end
